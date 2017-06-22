b773131690bcbb0b8cab03d2bc3ba729e7e49048
andriy <andriystasenko22@gmail.com>
2017-06-22 17:49:21 +0300

    first commit

--- app/js/comon.js
+++ app/js/comon.js
@@ -3,9 +3,25 @@ $(document).ready(function() {
 	var menu_height = $('.navbar').outerHeight(); // висота меню
 	new WOW().init();
 
+	var phone_slider = $('.phone_slider');
+	phone_slider.owlCarousel({
+		items:1,
+		loop:true,
+		smartSpeed:550,
+	});
+
+	// кнопки слайдер "Наша команда"
+	$('.phone_slider_button .btn_next').click(function() {
+		phone_slider.trigger('next.owl.carousel');
+	});
+	$('.phone_slider_button .btn_prev').click(function() {
+		phone_slider.trigger('prev.owl.carousel');
+	});
+	
 	// шапка сайту
 	$(window).scroll(function(event) {
-		if($(window).scrollTop() > header_height){
+		onScroll();
+		if($(window).scrollTop() > header_height - menu_height){
 			$('.navbar').css('background-color', 'rgba(39,50,61, 1)');
 		}
 		else{
@@ -20,4 +36,23 @@ $(document).ready(function() {
 		var block_position = $(block).offset().top - menu_height;
 		$('html, body').animate({scrollTop : block_position}, 800);
 	});
+
+
+ 	$('.fancy').fancybox();
 });
+
+function onScroll(){
+	var menu_height = $('.navbar').outerHeight();
+	var scroll_top = $(window).scrollTop();
+	$(".menu a").each(function(){
+		var hash = $(this).attr("href");
+		var target = $(hash);
+		var position = target.position().top - menu_height;
+		if (position  <= scroll_top && position  + target.outerHeight() > scroll_top) {
+			$(".menu a.active").removeClass("active");
+			$(this).addClass("active");
+		} else {
+			$(this).removeClass("active");
+		}
+	});
+}
